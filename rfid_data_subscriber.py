2d75082b224318154ce9fb065a443f548b33070c
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-09-07 22:29:45 +0530

    working

--- rfid_data_subscriber.py
+++ rfid_data_subscriber.py
@@ -1,199 +1,85 @@
-import paho.mqtt.client as mqtt
 import json
-from pymongo import MongoClient
-import openpyxl
+import csv
 
-# Define the file path for the Excel file
-file_path = "HHDG - SpareInventory - excel.xlsx"
+from pymongo import MongoClient
+import paho.mqtt.client as mqtt
+
 
 # Connect to the MongoDB database
 db_client = MongoClient("mongodb://127.0.0.1:27017/")
+db = db_client["Product_Management"]
+collection = db["products"]
 
-# Loading the Excel file using openpyxl
-try:
-    workbook = openpyxl.load_workbook(file_path)
-    print("Workbook loaded successfully.")
-except FileNotFoundError:
-    print(f"The file '{file_path}' does not exist.")
-except Exception as e:
-    print(f"An error occurred while opening the file: {str(e)}")
+file_path = "HHDG - SpareInventory.xlsx - HHDG - SpareInventory.csv"
 
-# MQTT Connection Callbacks
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected to MQTT broker\nlistening for info...")
+        print("Connected to MQTT broker\nlistening for info on ...")
+        connected = True
         client.subscribe('Inbound')
         print("Subscribed to 'Inbound' topic")
         client.subscribe('Outbound')
         print("Subscribed to 'Outbound' topic")
     else:
         print("Connection failed")
+        connected = False
 
-# MQTT Message Callback
 def on_message(client, userdata, message):
-    payload = message.payload.decode('utf-8')
+    payload = message.payload.decode("utf-8")
     data = json.loads(payload)
-    extracted_data = parse_data(data)
-    append_to_database(data, extracted_data)
 
-# Function to append data to the MongoDB database
-# def append_to_database(data, extracted_data):
-    # db = db_client["Product_Management"]
-    # collection = db["products"]
-    #
-    # # Filter to find a document with the same "Location" and "Box" values
-    # filter_query = {
-    #     "Location": data.get("Location", ""),
-    #     "Box": data.get("Box", ""),
-    # }
-    #
-    # # Try to find an existing document
-    # existing_document = collection.find_one(filter_query)
-    #
-    # if existing_document:
-    #     existing_rfid_entries = existing_document.get("RFID", [])
-    #
-    #     existing_rfid_entries_map = {entry.get("PRODUCT"): index for index, entry in enumerate(existing_rfid_entries)}
-    #
-    #     # Update or add new RFID entries
-    #     for rfid_entry in data.get("RFID", []):
-    #         product_id = rfid_entry.get("PRODUCT")
-    #         extracted_data_entry = extracted_data.get(product_id, {})
-    #
-    #         if product_id in existing_rfid_entries_map:
-    #             index = existing_rfid_entries_map[product_id]
-    #             existing_rfid_entries[index].update({**rfid_entry, **extracted_data_entry})
-    #         else:
-    #             existing_rfid_entries.append({**rfid_entry, **extracted_data_entry})
-    #
-    #     collection.update_one(filter_query, {"$set": {"RFID": existing_rfid_entries}})
-    #     print(f"Updated existing document for {data.get('Box', '')} in the database")
-    # else:
-    #     # Create a new entry
-    #     new_entry = {
-    #         "Location": data.get("Location", ""),
-    #         "Box": data.get("Box", ""),
-    #         "RFID": [
-    #             {**rfid_entry, **extracted_data.get(rfid_entry["PRODUCT"], {})}
-    #             for rfid_entry in data.get("RFID", [])
-    #         ]
-    #     }
-    #     collection.insert_one(new_entry)
-    #     print(f"Added new document for {data.get('Box', '')} to the database")
-    # Function to append data to the MongoDB database
+    if message.topic == "Inbound":
+        handle_inbound(data=data)
 
-# Function to append data to the MongoDB database
-def append_to_database(data, extracted_data):
-    db = db_client["Product_Management"]
-    collection = db["products"]
+    if message.topic == "Outbound":
+        handle_outbound(data=data)
 
-    # Extract location and box from the data
-    location = data.get("Location", "")
-    box = data.get("Box", "")
-    product_ids = [entry["PRODUCT"] for entry in data["RFID"]]
-
-    # Filter to find all documents containing any of the product IDs in the RFID data
-    filter_query = {
-        "RFID.PRODUCT": {"$in": product_ids},
-    }
-
-    # Find all documents matching the filter query
-    matching_documents = collection.find(filter_query)
-
-    for existing_document in matching_documents:
-        existing_rfid_entries = existing_document.get("RFID", [])
-
-        # Create a set to keep track of existing product IDs in this box
-        existing_product_ids = set(entry.get("PRODUCT") for entry in existing_rfid_entries)
-
-        # Check if all new products match the existing ones
-        all_new_products_exist = all(product_id in existing_product_ids for product_id in product_ids)
-
-        if all_new_products_exist:
-            # If all new products already exist in this box, delete the entire entry
-            collection.delete_one({"_id": existing_document["_id"]})
-            print(f"Deleted existing document for {existing_document['Box']} at {existing_document['Location']} from the database")
-        else:
-            # Create a list to store updated RFID entries
-            updated_rfid_entries = []
-
-            # Update or add new RFID entries
-            for rfid_entry in data.get("RFID", []):
-                product_id = rfid_entry.get("PRODUCT")
-                extracted_data_entry = extracted_data.get(product_id, {})
-
-                if product_id in existing_product_ids:
-                    # If the product already exists in this box, remove it
-                    existing_rfid_entries = [entry for entry in existing_rfid_entries if entry.get("PRODUCT") != product_id]
-                else:
-                    # Check if the product exists in other boxes at the same location
-                    for existing_box_document in collection.find({
-                        "Location": location,
-                        "Box": {"$ne": box},
-                        "RFID.PRODUCT": product_id
-                    }):
-                        # Remove the product from other boxes at the same location
-                        existing_box_rfid_entries = existing_box_document.get("RFID", [])
-                        existing_box_rfid_entries = [entry for entry in existing_box_rfid_entries if
-                                                     entry.get("PRODUCT") != product_id]
-                        # Update the existing box document
-                        collection.update_one({"_id": existing_box_document["_id"]},
-                                              {"$set": {"RFID": existing_box_rfid_entries}})
-
-                    # Add the new RFID entry to this box
-                    updated_rfid_entries.append({**rfid_entry, **extracted_data_entry})
-
-            # Extend the existing entries with the updated RFID entries
-            existing_rfid_entries.extend(updated_rfid_entries)
-
-            # Update the existing box document
-            collection.update_one({"_id": existing_document["_id"]}, {"$set": {"RFID": existing_rfid_entries}})
-            print(f"Updated existing document for {existing_document['Box']} at {existing_document['Location']} in the database")
-
-    # If no matching documents are found, create a new entry
-    new_entry_query = {
-        "Location": location,
-        "Box": box,
-    }
-    if not collection.find_one(new_entry_query):
-        new_entry = {
-            "Location": location,
-            "Box": box,
-            "RFID": [
-                {**rfid_entry, **extracted_data.get(rfid_entry["PRODUCT"], {})}
-                for rfid_entry in data.get("RFID", [])
-            ]
+def handle_inbound(data):
+    for rfid_item in data["RFID"]:
+        product = rfid_item | extract_info(product_id=f"VS.{rfid_item['COMPANY']}.{rfid_item['PRODUCT']}") | {
+            "Zone": data["Location"],
+            "Box": "B-{}".format(data["Location"].split("-")[2]),
+            "ZONE": int(data["Location"].split("-")[0].replace("Zone", "")),
+            "LEVEL": int(data["Location"].split("-")[1]),
+            "BOX": data["Location"].split("-")[2],
         }
-        collection.insert_one(new_entry)
-        print(f"Added new document for {box} at {location} to the database")
 
+        product_id_filter = {"PRODUCT": f"VS.{rfid_item['COMPANY']}.{rfid_item['PRODUCT']}"}
+        collection.update_one(product_id_filter, {"$set": product}, upsert=True)
+    print("Documents Updated/ Inserted")
 
-# Function to process product data
-def process(product_id):
-    sheet = workbook["main"]
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        mach_desc, maker_desc, material, material_desc, part_no, rob = row[2:8]
-        if material and material.endswith(product_id):
-            return {
-                "MACH_DESC": mach_desc,
-                "MAKER_DESC": maker_desc,
-                "MATERIAL": material,
-                "MATERIAL_DESC": material_desc,
-                "PART_NO": part_no,
-                "ROB": rob
-            }
-    return None
+def handle_outbound(data):
+    [collection.delete_one({"PRODUCT": f"VS.{rfid_item['COMPANY']}.{rfid_item['PRODUCT']}"}) for rfid_item in data["RFID"]]
+    print("Documents Deleted")
 
-# Function to parse incoming data and extract information
-def parse_data(data):
-    extracted_data = {}  # Storing extracted data for all RFID entries
-    for rfid_entry in data.get("RFID", []):
-        product_id = rfid_entry.get("PRODUCT")
-        extracted_data_entry = process(product_id)
-        if extracted_data_entry:
-            extracted_data[product_id] = extracted_data_entry
+def extract_info(product_id):
+    with open(file_path, mode='r') as file:
+        csv_reader = csv.DictReader(file)
 
-    return extracted_data  # Extracted data for all RFID entries
+        for row in csv_reader:
+            match = row["MATERIAL"]
+
+            if product_id == match:
+                matched_item = {
+                    'PRODUCT': product_id,
+                    'MACH_DESC': row['MACH_DESC'],
+                    'MAKER_DESC': row['MAKER_DESC'],
+                    'MATERIAL': row['MATERIAL'],
+                    'MATERIAL_DESC': row['MATERIAL_DESC'],
+                    'PART_NO': row['PART_NO'],
+                    'ROB': row['ROB'],
+                }
+                return matched_item
+
+    matched_item = {
+        'PRODUCT': product_id,
+        'MACH_DESC': "not found",
+        'MAKER_DESC': "not found",
+        'MATERIAL': "not found",
+        'PART_NO': "not found",
+        'ROB': "not found",
+    }
+    return matched_item
 
 
 # MQTT Broker Configuration
@@ -201,10 +87,11 @@ mqttBroker = "mqtt.eclipseprojects.io"
 # mqttBroker = "192.168.1.20"
 
 # MQTT Client Configuration
-client = mqtt.Client("data")
+client = mqtt.Client("RFID", clean_session=False)
+print("MQTT Broker: {}".format(mqttBroker))
 client.on_connect = on_connect
 client.on_message = on_message  # Message listener
 
-# Connect to the MQTT broker
 client.connect(mqttBroker)
+
 client.loop_forever()
--- test_publisher.py
+++ test_publisher.py
@@ -10,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
         print("Connection failed")
 
 
-client = mqtt.Client()
+client = mqtt.Client("publish", clean_session=False)
 client.on_connect = on_connect
 broker_address = "mqtt.eclipseprojects.io"
 # mqttBroker = "192.168.1.20"
@@ -65,27 +65,33 @@ client.connect(broker_address)
 def send_item_data():
     zone = int(input('Enter Zone: '))
     level = int(input('Enter Level: '))
-    box = int(input('Enter Box: '))
+    box = input('Enter Box: ')
     scan = {
         "TimeStamp": "2023-02-15 14:30:45",
         "Device": "PDA2",
         "Location": f"Zone{zone}-{level}-{box}",
         "Box": f"B-{box}",
         "RFID": [
-            {"SERIALNO": "45324335", "PRODUCT": "9014386", "EPC": "301A94B9E2631C8002B3982F", "COMPANY": "CAS"},
-            {"SERIALNO": "45383536", "PRODUCT": "9013784", "EPC": "301A94B9E262860002B47F70", "COMPANY": "CAS"},
-            {"SERIALNO": "45373638", "PRODUCT": "9013780", "EPC": "301A94B9E262850002B458C6", "COMPANY": "CAS"},
-            {"SERIALNO": "45314431", "PRODUCT": "9014389", "EPC": "301A94B9E2631D4002B3717F", "COMPANY": "CAS"},
-            {"SERIALNO": "45373637", "PRODUCT": "9013785", "EPC": "301A94B9E262864002B458C5", "COMPANY": "CAS"},
-            {"SERIALNO": "45314431", "PRODUCT": "9014365", "EPC": "301A94B9E263174002B3717F", "COMPANY": "CAS"},
-            {"SERIALNO": "45324336", "PRODUCT": "9014364", "EPC": "301A94B9E263170002B39830", "COMPANY": "CAS"},
-            {"SERIALNO": "45334334", "PRODUCT": "9014393", "EPC": "301A94B9E2631E4002B3BF3E", "COMPANY": "CAS"},
-            {"SERIALNO": "45373636", "PRODUCT": "9013795", "EPC": "301A94B9E26288C002B458C4", "COMPANY": "CAS"},
-            {"SERIALNO": "45383537", "PRODUCT": "9013782", "EPC": "301A94B9E262858002B47F71", "COMPANY": "CAS"}
+            {"SERIALNO": "45324335", "PRODUCT": "9008143", "EPC": "301A94B9E2631C8002B3982F", "COMPANY": "BWS"},
+            {"SERIALNO": "45383536", "PRODUCT": "9008144", "EPC": "301A94B9E262860002B47F70", "COMPANY": "BWS"},
+            {"SERIALNO": "45373638", "PRODUCT": "9008145", "EPC": "301A94B9E262850002B458C6", "COMPANY": "BWS"},
+            {"SERIALNO": "45314431", "PRODUCT": "9008146", "EPC": "301A94B9E2631D4002B3717F", "COMPANY": "BWS"},
+            {"SERIALNO": "45373637", "PRODUCT": "9008147", "EPC": "301A94B9E262864002B458C5", "COMPANY": "BWS"},
+            {"SERIALNO": "45314431", "PRODUCT": "9008148", "EPC": "301A94B9E263174002B3717F", "COMPANY": "BWS"},
+            {"SERIALNO": "45324336", "PRODUCT": "9008149", "EPC": "301A94B9E263170002B39830", "COMPANY": "BWS"},
+            {"SERIALNO": "45334334", "PRODUCT": "9008150", "EPC": "301A94B9E2631E4002B3BF3E", "COMPANY": "BWS"},
+            {"SERIALNO": "45373636", "PRODUCT": "9008151", "EPC": "301A94B9E26288C002B458C4", "COMPANY": "BWS"},
+            {"SERIALNO": "45383537", "PRODUCT": "9008152", "EPC": "301A94B9E262858002B47F71", "COMPANY": "BWS"},
+            {"SERIALNO": "45324335", "PRODUCT": "9015365", "EPC": "301A94B9E2631C8002B3982F", "COMPANY": "CAS"},
         ]
     }
     message = json.dumps(scan)
-    topic = choice(["Inbound", "Outbound"])
+
+    if int(input("Enter 0 for inbound and 1 for outbound.")) == 1:
+        topic = "Outbound"
+    else:
+        topic = "Inbound"
+
     client.publish(topic, message)
     print(message)
 
@@ -96,3 +102,5 @@ while True:
         send_item_data()
     else:
         break
+
+client.loop_forever()
